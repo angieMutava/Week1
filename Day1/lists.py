65a33415e2373bae3299f0dca3bf2a9f68f7d876
angieMutava <mutavaangela@yahoo.com>
2017-02-07 08:52:03 -0800

    added challenge

--- Day1/lists.py
+++ Day1/lists.py
@@ -13,3 +13,23 @@ lists.reverse()
 created_list = [(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
 for item in created_list:
 	print item
+
+###Challenge
+'''
+Write a function that takes a list of strings an prints them, one per line, in a rectangular frame. For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
+
+*********
+* Hello *
+* World *
+* in    *
+* a     *
+* frame *
+*********
+'''
+
+lists = ["Hello", "World", "in", "a", "frame"]
+print("*******")
+for item in lists:
+  print('*'+ item, ''+'*' )
+print("*******")
+
